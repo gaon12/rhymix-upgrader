040f11d3014572a529b31923e77f2ec6bd5935ec
gaon12 <34927797+gaon12@users.noreply.github.com>
2023-03-29 13:10:19 +0900

    링크 스타트!

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# rhymix-upgrader
+# 본 자료는
+[라이믹스](https://github.com/rhymix/rhymix)를 최신버전으로 업데이트 시켜주는 PHP 파일입니다.
+
+# 사용 방법
+1. upgrader.php 파일을 루트 디렉토리(index.php가 있는 곳)에 저장해 주세요.
+2. 혹시 모르니 백업은 필수!
+3. 현재 사용중인 버전과 최신버전을 확인하고 Upgrade 버튼을 눌러주세요!
+4. 완료 메시지가 뜨면 끝!
+
+# 원리
+<code>./common/constants.php</code> 파일에서 현재 사용중인 버전을 확인합니다. 이후 GitHub API를 이용하여 최신버전 값을 읽은 뒤, 둘을 비교하여 버전이 다른 경우 최신 버전 zip 파일을 다운로드 후 압축 해제 하고 덮어씌웁니다.
+
+# 주의사항
+* 항상 실행 전 백업을 합시다!
+* 또한 다음 PHP 라이브러리가 설치되어 있어야 합니다.
+  - cURL extension (HTTP 요청을 위한 PHP 확장 기능)
+  - ZipArchive extension (ZIP 파일 압축 및 해제를 위한 PHP 확장 기능)
+* 또한 디렉토리에 쓰기 권한이 있어야 합니다.
+
+# 라이선스
+MIT 라이선스, GPL v3 둘 중 원하는 라이선스를 선택하세요! 저는 MIT를 권장합니다.
---
+++ upgrader.php
@@ -0,0 +1,221 @@
+<?php
+
+function rrmdir($src)
+{
+    $dir = opendir($src);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            $full = $src . '/' . $file;
+            if (is_dir($full)) {
+                rrmdir($full);
+            } else {
+                unlink($full);
+            }
+        }
+    }
+    closedir($dir);
+    rmdir($src);
+}
+
+function recurse_copy($src, $dst)
+{
+    $dir = opendir($src);
+    @mkdir($dst);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                recurse_copy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+    closedir($dir);
+}
+
+function getCurrentVersion()
+{
+    $current_version = '';
+    $file = file_get_contents('./common/constants.php');
+    preg_match("/define\('RX_VERSION', '([^']+)'\);/", $file, $matches);
+    if (!empty($matches[1])) {
+        $current_version = $matches[1];
+    }
+    return $current_version;
+}
+
+function getLatestVersion()
+{
+    $latest_version = '';
+    $api_url = 'https://api.github.com/repos/rhymix/rhymix/tags';
+    $options = ['http' => ['header' => ['User-Agent: PHP']]];
+    $context = stream_context_create($options);
+    $response = file_get_contents($api_url, false, $context);
+    $tags = json_decode($response);
+
+    if (!empty($tags[0]->name)) {
+        $latest_version = $tags[0]->name;
+    }
+    return $latest_version;
+}
+
+function downloadFile($url, $path)
+{
+    $content = file_get_contents($url);
+    file_put_contents($path, $content);
+}
+
+function upgradeVersion($latest_version)
+{
+    $result = [];
+
+    // Download the latest version
+    $download_url = "https://github.com/rhymix/rhymix/archive/refs/tags/{$latest_version}.zip";
+    $zip_path = "{$latest_version}.zip";
+    downloadFile($download_url, $zip_path);
+
+    // Unzip the archive
+    $zip = new ZipArchive;
+    $zip->open($zip_path);
+    $zip->extractTo('.');
+    $zip->close();
+
+    // Copy files from the extracted folder
+    $src = "rhymix-{$latest_version}";
+    $dest = '.';
+    recurse_copy($src, $dest);
+
+    // Remove the zip file and extracted folder
+    unlink($zip_path);
+    rrmdir($src);
+
+    $result['status'] = 'success';
+    $result['message'] = 'Upgrade Complete';
+    return $result;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $latest_version = getLatestVersion();
+    $result = upgradeVersion($latest_version);
+    header('Content-Type: application/json');
+    echo json_encode($result);
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rhymix Upgrader</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f7f7f7;
+        }
+
+        .container {
+            padding: 2rem;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+            width: 500px;
+            height: 300px;
+        }
+
+        .title {
+            text-align: center;
+        }
+
+        .version {
+            margin-left: 15px;
+        }
+
+        button {
+            background-color: #007bff;
+            border: none;
+            color: #fff;
+            padding: 0.5rem 1rem;
+            font-size: 1rem;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: auto;
+            display: block;
+        }
+
+        .progress {
+            display: none;
+            margin-top: 1rem;
+        }
+
+        .progress-bar {
+            background-color: #007bff;
+            height: 1rem;
+            width: 0;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="title">Rhymix Upgrader</h1>
+        <p class="version">Current version:
+            <?= getCurrentVersion(); ?>
+        </p>
+        <p class="version">Latest version:
+            <?= getLatestVersion(); ?>
+        </p>
+        <br><br>
+        <button type="button" onclick="checkVersionsAndUpgrade()">Upgrade</button>
+        <div class="progress">
+            <div class="progress-bar"></div>
+        </div>
+        <p id="upgradeStatus"></p>
+    </div>
+    <script>
+        function checkVersionsAndUpgrade() {
+        const currentVersion = '<?= getCurrentVersion(); ?>';
+        const latestVersion = '<?= getLatestVersion(); ?>';
+
+        if (currentVersion === latestVersion) {
+            const shouldUpgrade = confirm('현재 최신버전을 사용중인 것 같습니다. 그래도 최신 버전으로 덮어씌우시겠습니까?');
+            if (!shouldUpgrade) {
+                return;
+            }
+        }
+
+        const upgradeBtn = document.querySelector('button');
+        upgradeBtn.disabled = true;
+        document.querySelector('.progress').style.display = 'block';
+
+        fetch('', { method: 'POST' })
+            .then(response => response.json())
+            .then(result => {
+                document.getElementById('upgradeStatus').innerText = result.message;
+
+                if (result.status === 'success') {
+                    document.querySelector('.progress-bar').style.width = '100%';
+                    setTimeout(function () {
+                        location.reload();
+                    }, 1000);
+                } else {
+                    upgradeBtn.disabled = false;
+                    document.querySelector('.progress').style.display = 'none';
+                }
+            });
+    }
+    </script>
+</body>
+
+</html>
